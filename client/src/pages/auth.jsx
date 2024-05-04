0228ed586a3ff1ea13cbc76a8e293492d60ae3f4
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-05-04 08:52:10 +0100

    added login/register, logout

--- client/src/pages/auth.jsx
+++ client/src/pages/auth.jsx
@@ -1,5 +1,7 @@
 import React,{useState} from 'react'
 import axios from "axios"
+import {useCookies} from "react-cookie"
+import { useNavigate } from 'react-router-dom'
 
 const Auth = () => {
   return (
@@ -15,13 +17,33 @@ const Login = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
+  const [, setCookies] = useCookies("access_token")
+
+  const navigate = useNavigate()
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    try{
+      const response = await axios.post("http://localhost:5174/auth/login", {
+        username,
+        password,
+      })
+
+      setCookies("access_token", response.data.token)
+      window.localStorage.setItem("useID", response.data.userID)
+      navigate("/")
+    } catch(err){
+      console.error(err)
+    }
+  };
+
   return (<Form 
     username={username} 
     setUsername={setUsername} 
     password={password} 
     setPassword={setPassword}
     label="Login"
-    // onSubmit={onSubmit}
+    onSubmit={onSubmit}
     />
     )
 }
